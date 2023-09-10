bdbd2eb9cc4da1bdf3de6e19ab9647b14049ed50
ThePhaseless <kukubaorch@gmail.com>
2023-09-10 21:54:05 +0200

    fixed 32 character cap crash

--- bot.js
+++ bot.js
@@ -11,7 +11,7 @@ client.on('ready', () => {
   client.user.setPresence({
     status: 'online',
     activity: {
-      name: 'jak umilić Ci życie',
+      name: 'jak przeżyć na studiach',
       type: 'WATCHING',
     },
   });
@@ -22,7 +22,7 @@ client.on('message', (message) => {
     // log the message
     // add the message to the senders nickname
     var nickname = message.member.nickname;
-    var inicial = ""
+    var inicial = "";
     // if the message has at least 2 words, use the first word and the first letters of the rest of the words
     if (message.content.split(' ').length > 1) {
       var words = message.content.split(' ');
@@ -38,7 +38,17 @@ client.on('message', (message) => {
 
     // log the message
     console.log(`${nickname} inicial is ${inicial}`);
+    
+    var newnickname = inicial + ' | ' + nickname;
+    if (newnickname.length > 32) {
+      newnickname = newnickname.substring(0, 32);
 
+      // log the message
+      console.log(`${nickname}s nickname is too long, shortening to ${newnickname}`);
+
+      // send a dm to the user
+      message.author.send(`Twój nick z initialami jest za długi, skracam do ${newnickname}`);
+    }
     // set the nickname
     message.member.setNickname(inicial + ' | ' + nickname);
     message.member.roles.add(ROLE_ID);
